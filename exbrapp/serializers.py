2b6b6463b127289c1c130407edfd24ea4f37dd53
etech <pythexcel@excellencetechnologies.in>
2019-07-22 17:54:18 +0530

    update serializer fix

--- exbrapp/serializers.py
+++ exbrapp/serializers.py
@@ -56,12 +56,14 @@ class ExhibitorSerializer(serializers.ModelSerializer):
 
     def update(self, instance, validated_data):
         print(instance)
-        instance.size = validated_data.get('size',instance.size)
-        instance.stall_no = validated_data.get('stall_no',instance.stall_no)
-        instance.color_theme = validated_data.get('color_theme',instance.color_theme)
-        instance.carpet = validated_data.get('carpet',instance.carpet)
-        instance.extra = validated_data.get('extra',instance.extra)
-        instance.website_link = validated_data.get('website_link',instance.website_link)
+        instance.size = validated_data.get('size', instance.size)
+        instance.stall_no = validated_data.get('stall_no', instance.stall_no)
+        instance.color_theme = validated_data.get('color_theme',
+                                                  instance.color_theme)
+        instance.carpet = validated_data.get('carpet', instance.carpet)
+        instance.extra = validated_data.get('extra', instance.extra)
+        instance.website_link = validated_data.get('website_link',
+                                                   instance.website_link)
         instance.save()
 
         furniture_data = validated_data.get('furnitures')
@@ -69,27 +71,24 @@ class ExhibitorSerializer(serializers.ModelSerializer):
         product_data = validated_data.get('products')
 
         for data in furniture_data:
-            data_id = data.get('id',None)
+            data_id = data.get('id', None)
             if data_id:
-                fur_data = FurnitureExhibitorDetail.objects.get(id=data_id,furnitures = instance)
+                fur_data = FurnitureExhibitorDetail.objects.get(
+                    id=data_id, furnitures=instance)
                 fur_data.furniture = data.get('furniture', fur_data.furniture)
                 fur_data.save()
         for elem in branding_data:
-            elem_id = elem.get('id',None)
+            elem_id = elem.get('id', None)
             if elem_id:
-                brd_data = BrandingExhibitorDetail.objects.get(id=elem_id,brandings = instance)
+                brd_data = BrandingExhibitorDetail.objects.get(
+                    id=elem_id, brandings=instance)
                 brd_data.branding = data.get('branding', brd_data.branding)
                 brd_data.save()
         for detail in product_data:
-            detail_id = detail.get('id',None)
+            detail_id = detail.get('id', None)
             if detail_id:
-                pro_data = ProductExhibitorDetail.objects.get(id=detail_id,products = instance)
+                pro_data = ProductExhibitorDetail.objects.get(
+                    id=detail_id, products=instance)
                 pro_data.products = data.get('products', pro_data.products)
                 pro_data.save()
-        return instance            
-
-
-
-
-
-        
+        return instance
