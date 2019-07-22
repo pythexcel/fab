62c63cd2c01a9d90cff642e12648f1cadf2046b5
etech <pythexcel@excellencetechnologies.in>
2019-07-22 17:02:00 +0530

    update serializer

--- exbrapp/serializers.py
+++ exbrapp/serializers.py
@@ -1,22 +1,28 @@
 from exbrapp.models import (Exhibitor, ProductExhibitorDetail,
-                             BrandingExhibitorDetail, FurnitureExhibitorDetail)
+                            BrandingExhibitorDetail, FurnitureExhibitorDetail)
 from rest_framework import serializers
 from fabapp.serializers import UserDetailSerializer, ExhibitionDetail
 
 
 class FurnituredDetail(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
+
     class Meta:
         model = FurnitureExhibitorDetail
         fields = ('id', 'furniture')
 
 
 class BrandingDetail(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
+
     class Meta:
         model = BrandingExhibitorDetail
         fields = ('id', 'branding')
 
 
 class ProductDetail(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
+
     class Meta:
         model = ProductExhibitorDetail
         fields = ('id', 'product')
@@ -49,14 +55,41 @@ class ExhibitorSerializer(serializers.ModelSerializer):
         return exi
 
     def update(self, instance, validated_data):
-        furniture_data = validated_data.pop('furnitures')
-        branding_data = validated_data.pop('brandings')
-        product_data = validated_data.pop('products')
-        instance.size = validated_data.get('size', instance.size)
-        instance.stall_no = validated_data.get('stall_no', instance.stall_no)
-        instance.color_theme = validated_data.get('color_theme',
-                                                  instance.color_theme)
-        instance.carpet = validated_data.get('carpet', instance.carpet)
-        instance.extra = validated_data.get('extra', instance.extra)
+        print(instance)
+        instance.size = validated_data.get('size',instance.size)
+        instance.stall_no = validated_data.get('stall_no',instance.stall_no)
+        instance.color_theme = validated_data.get('color_theme',instance.color_theme)
+        instance.carpet = validated_data.get('carpet',instance.carpet)
+        instance.extra = validated_data.get('extra',instance.extra)
+        instance.website_link = validated_data.get('website_link',instance.website_link)
         instance.save()
-        return instance
+
+        furniture_data = validated_data.get('furnitures')
+        branding_data = validated_data.get('brandings')
+        product_data = validated_data.get('products')
+
+        for data in furniture_data:
+            data_id = data.get('id',None)
+            if data_id:
+                fur_data = FurnitureExhibitorDetail.objects.get(id=data_id,furnitures = instance)
+                fur_data.furniture = data.get('furniture', fur_data.furniture)
+                fur_data.save()
+        for elem in branding_data:
+            elem_id = elem.get('id',None)
+            if elem_id:
+                brd_data = BrandingExhibitorDetail.objects.get(id=elem_id,brandings = instance)
+                brd_data.branding = data.get('branding', brd_data.branding)
+                brd_data.save()
+        for detail in product_data:
+            detail_id = detail.get('id',None)
+            if detail_id:
+                pro_data = ProductExhibitorDetail.objects.get(id=detail_id,products = instance)
+                pro_data.products = data.get('products', pro_data.products)
+                pro_data.save()
+        return instance            
+
+
+
+
+
+        
