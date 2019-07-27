d49ffb2607224b6d7890779d26fd024585d908e3
etech <pythexcel@excellencetechnologies.in>
2019-07-27 15:05:14 +0530

    list exi

--- fab/settings.py
+++ fab/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'fab',
         'USER': 'root',
-        'PASSWORD': '123',  #java@123
+        'PASSWORD': 'java@123',  #java@123
         'HOST': 'localhost',
         'PORT': '',
     }
