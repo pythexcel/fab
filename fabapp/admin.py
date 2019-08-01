638bda4503481e12315ecb89fa1fcd3376bf46c2
etech <pythexcel@excellencetechnologies.in>
2019-08-01 15:12:04 +0530

    admin css fix

--- fabapp/admin.py
+++ fabapp/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
+from fabapp.models import Exhibition,ExhibitFab,AvailBrand,AvailFurni,AvailProd,User,Exhibition
+from exbrapp.models import Bid
 
-from fabapp.models import User, Exhibition
 admin.site.register(User)
 admin.site.register(Exhibition)
-
+admin.site.register(ExhibitFab)
+admin.site.register(AvailBrand)
+admin.site.register(AvailFurni)
+admin.site.register(AvailProd)  
+admin.site.register(Bid)
