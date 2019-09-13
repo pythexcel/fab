3c339b67bbd051f7a693be3ebc2f1fcd5735537f
etech <pythexcel@excellencetechnologies.in>
2019-09-13 19:04:47 +0530

    some fcm fixes

--- exbrapp/views.py
+++ exbrapp/views.py
@@ -107,6 +107,9 @@ class CreateBid(APIView):
         exhibition_name = exi_ser.data['exhibition']['exhibition_name']
         print(exhibhition.id)
         ser = UserDetailSerializer(fab_user,many=False)
+        devices = FCMDevice.objects.get(user=ser.data['id'])
+        devices.send_message(title="Notification from "+ self.request.user + "You have beed Invited for "+ exhibition_name,
+             body="")
         bid = Bid.objects.filter(fabs_user_id=fab_user.id,mine_exhib_id=exhibhition.id)
         if not bid:
             bid = Bid(fabs_user_id=fab_user.id,mine_exhib_id=exhibhition.id)
@@ -117,9 +120,6 @@ class CreateBid(APIView):
              body="")
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
-            devices = FCMDevice.objects.get(user=ser.data['id'])
-            devices.send_message(title="Notification from "+ self.request.user + "You have beed Invited for "+ exhibition_name,
-             body="")
             return Response({"is_already_added": True})
 
     def put(self,request,format=None,pk=None):
